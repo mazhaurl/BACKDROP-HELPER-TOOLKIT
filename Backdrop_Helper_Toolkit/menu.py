4c89ad08c376404e88250d9bd8caf606e63c3bb1
Md Mazharul Islam <52231250+mazhaurl@users.noreply.github.com>
2024-07-18 00:28:31 +0600

    Update menu.py

--- Backdrop_Helper_Toolkit/menu.py
+++ Backdrop_Helper_Toolkit/menu.py
@@ -1,1 +1,9 @@
 
+
+from nukescripts import panels
+import Backdrop_Helper_Toolkit
+
+
+panels.registerWidgetAsPanel('Backdrop_Helper_Toolkit.backdrop_helper_toolkit', 'Backdrop Helper Toolkit', 'www.shuvofx.com')
+
+
